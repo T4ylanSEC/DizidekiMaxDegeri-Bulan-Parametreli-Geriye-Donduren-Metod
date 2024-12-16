75be5585e513d739156da5b475e651616e655265
yaren <yaren@DESKTOP-9U2BG79>
2024-12-17 01:37:56 +0300

    Add project files.

---
+++ DizidekiMaxDegeri Bulan Parametreli Geriye Donduren Metod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DizidekiMaxDegeri_Bulan_Parametreli_Geriye_Donduren_Metod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] sayilar = { 12, 96, 7, 8, 128, 35, 25, 6666, 35, 10, 958, 416, 1298, 25, 256};
+
+            int enBuyuk = EnBuyukDegeriBul(sayilar);
+            Console.WriteLine("Dizideki En Büyük Değer: " + enBuyuk);
+
+            Console.ReadKey();
+        }
+
+        static int EnBuyukDegeriBul(int[] dizi)
+        {
+            if (dizi == null || dizi.Length == 0)
+                throw new ArgumentException("Dizi boş veya sıfır olamaz.");
+
+            int enBuyuk = dizi[0];
+
+            foreach (int sayi in dizi)
+            {
+                if (sayi > enBuyuk)
+                {
+                    enBuyuk = sayi;
+                }
+            }
+
+            return enBuyuk;
+
+        }
+    }
+}
---
+++ DizidekiMaxDegeri Bulan Parametreli Geriye Donduren Metod/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DizidekiMaxDegeri Bulan Parametreli Geriye Donduren Metod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DizidekiMaxDegeri Bulan Parametreli Geriye Donduren Metod")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59b3b617-27d9-469e-8c72-4ae4fad0b658")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
